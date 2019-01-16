89ce41f029a0de9bfdc5bbba9fb9c47eb6a6439b
浅忆梦微凉 <3538922590@qq.com>
2019-01-16 11:28:25 +0800

    编译通过

---
+++ 剑指offer10_链表中倒数第k个结点/test.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<assert.h>
+#include<list>
+
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {
+	}
+};
+
+typedef int DataType;
+
+class Solution {
+public:
+	Solution()
+		:_phead(nullptr)
+	{}
+
+	ListNode* BuyNewNode(DataType val){
+		ListNode * NewNode = (ListNode*)new ListNode(val);
+		return NewNode;
+	}
+	void PushFront(DataType data){
+		ListNode* NewNode = BuyNewNode(data);
+		if (NewNode == nullptr){
+			assert(NewNode);
+			return;
+		}
+		NewNode->next = _phead;
+		_phead = NewNode;
+	}
+	ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+		ListNode* cur = pListHead;
+		ListNode* pre = pListHead;
+		int count = 0;
+		while (cur != nullptr&&count < k){
+			cur = cur->next;
+			++count;
+		}
+		if (count < k&&cur==nullptr){
+			return nullptr;
+		}
+		while (cur != nullptr){
+			cur = cur->next;
+			pre = pre->next;
+		}
+		return pre;
+	}
+	~Solution(){
+		ListNode* cur = _phead;
+		while (cur){
+			cur = cur->next;
+			delete _phead;
+			_phead = cur;
+		}
+	}
+public:
+	ListNode* _phead;
+};
+
+int main(){
+
+	system("pause");
+	return 0;
+}
